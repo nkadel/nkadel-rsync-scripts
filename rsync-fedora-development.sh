97a83c5c3138f7d98a999a1b0d1de4d6683db792
Nico Kadel-Garcia <nkadel@gmail.com>
2019-03-23 11:03:46 +0000

    Add distinct script for copying fedora development distinctly

---
+++ rsync-fedora-development.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#
+# fedora-mirror.sh - mirror Fedora
+# $Id: rsync-fedora.sh,v 1.20 2013/12/31 21:33:01 root Exp root $
+
+# Sort things correctly
+export LANG=C
+
+PROGNAME="$0"
+
+RSYNCARGS="$@"
+
+RSYNCARGS="$RSYNCARGS -aH"
+RSYNCARGS="$RSYNCARGS -v"
+#RSYNCARGS="$RSYNCARGS -v"
+#RSYNCARGS="$RSYNCARGS -q"
+RSYNCARGS="$RSYNCARGS --no-motd"
+RSYNCARGS="$RSYNCARGS -P"
+
+case "$RSYNCARGS" in
+    *--delete*)
+	echo Skipping --delete-after --delete-excluded selection with $@
+	;;
+    *)
+	#RSYNCARGS="$RSYNCARGS --delete"
+	RSYNCARGS="$RSYNCARGS --delete-after"
+	RSYNCARGS="$RSYNCARGS --delete-excluded"
+	;;
+esac
+
+RSYNCARGS="$RSYNCARGS --bwlimit=1000"
+#RSYNCARGS="$RSYNCARGS --dry-run"
+RSYNCARGS="$RSYNCARGS --timeout=60"
+RSYNCARGS="$RSYNCARGS --no-owner"
+RSYNCARGS="$RSYNCARGS --no-group"
+
+EXCLUDES="--exclude-from=$PWD/rsync-fedora-development.excludes"
+
+cd /var/www/linux/fedora-development || exit 1
+
+# Get latest version from development to live, if latest exists
+if [ ! -e ../fedora/releases/30 -a -d 30/ ]; then
+    mkdir ../fedora/releases/30
+    echo Mirroring 30/ to ../fedora/releases/30/
+    /bin/cp -a -l -n 30/. ../fedora/releases/30/.
+fi
+
+num=0
+while [ $num -lt 30 ]; do
+    rsync $RSYNCARGS \
+	  $EXCLUDES \
+	  rsync://mirrors.kernel.org/fedora/development/  ./ && break
+    num=`expr $num + 1`
+done

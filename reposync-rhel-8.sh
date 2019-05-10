e4b855e1733d818842d191af04a34f64667884ab
Nico Kadel-Garcia <nkadel@gmail.com>
2019-05-10 18:05:25 -0400

    Active --delete, disable RPM discarding hooks in reposync-rhel-8.sh

--- reposync-rhel-8.sh
+++ reposync-rhel-8.sh
@@ -25,7 +25,9 @@ rm -f $LOGFILE && \
 set -o pipefail
 
 REPOSYNCARGS=""
-REPOSYNCARGS="$REPOSYNCARGS --newest"
+#REPOSYNCARGS="$REPOSYNCARGS --newest"
+# Clean up after old package failures
+REPOSYNCARGS="$REPOSYNCARGS --delete"
 
 #REPOS=""
 #REPOS="$REPOS rhel-8-for-x86_64-baseos-rpms"
@@ -34,28 +36,28 @@ REPOSYNCARGS="$REPOSYNCARGS --newest"
 # "dnf repolist" contans undesired headers
 REPOS="`dnf -q repolist --disablerepo=* --enablerepo=rhel-8-* 2>/dev/null | grep ^rhel-8 | awk '{print $1}'`"
 
+cd $REPODIR || exit 1
 for repo in $REPOS; do
-    pushd $REPODIR || exit 1
+    echo
+    echo "$progname: mirroring $repo to $REPODIR/$repo"
     nice reposync \
         $REPOSYNCARGS \
 	--repoid=$repo 2>&1 | tee $LOGFILE
-    popd || exit 1
 done
 
-if [ $? -eq 0 ]; then
-    echo "Clearing out-of-date RPMs from: $REPODIR"
-    find $REPODIR -name \*.rpm | sort -u | while read name; do
-	#echo Checking $name
-	rpmname=`basename $name`
-	grep -q " Skipping existing $rpmname$" $LOGFILE && continue
-
-	echo "Flushing unsynced $name"
-	rm -f $name
-    done
-else
-    echo "Warning: reposync logged to $LOGFILE unsuccessful"
-    echo "    Not scrubbing mismaatched RPMs"
-fi
+#for repo in $REPOS; do
+#    echo "Clearing out-of-date RPMs from: $REPODIR"
+#    find $REPODIR/$repo/ -name \*.rpm | sort -u | while read name; do
+#	#echo Checking $name
+#	rpmname=`basename $name`
+#	grep -q "\[SKIPPED\] $rpmname: Already downloaded" $LOGFILE && continue
+#
+#	echo "Flushing unsynced $name"
+#	echo rm -f $name && continue
+#    done
+#    echo "Warning: reposync logged to $LOGFILE unsuccessful"
+#    echo "    Not scrubbing mismaatched RPMs"
+#done
 
 # Run createrepo even if reposync fails: risk of partial
 # update causing confusion has to be traded off against having

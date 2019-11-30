6762f56ff972056372120293f0e7df9bbdc8fc18
Nico Kadel-Garcia <nkadel@gmail.com>
2019-11-29 19:57:49 -0500

    Update filters

--- reposync-rhel-8.sh
+++ reposync-rhel-8.sh
@@ -33,7 +33,7 @@ export LANG=C
 
 progname=`basename $0`
 
-set -e
+#set -e
 set -o pipefail
 
 RSYNCARGS="$@"
@@ -50,7 +50,7 @@ REPOSYNCARGS=""
 # Clean up after old package failures
 REPOSYNCARGS="$REPOSYNCARGS --delete"
 # Download metadata
-REPOSYNCARGS="$REPOSYNCARGS --download-metadata"
+#REPOSYNCARGS="$REPOSYNCARGS --download-metadata"
 
 # Set repos manually
 #REPOS=""
@@ -59,20 +59,40 @@ REPOSYNCARGS="$REPOSYNCARGS --download-metadata"
 
 # Deduce subscribed repos. Repos are not even listed if not *subscribed*!!!
 # "dnf repolist" contans undesired headers
-REPOS="`subscription-manager repos --list | grep 'Repo ID:' | awk '{print $NF}' | LANG=C sort`"
+REPOS="`subscription-manager repos --list | grep 'Repo ID:' | awk '{print $NF}' | LANG=C sort -r`"
+
 
 # Filter out channels that are empty, or broken, on RHEL 8 subscriptions
 echo "Filtering REPOS for undesirable, satellite, and -7- channels"
 #REPOS="`echo "$REPOS" | sed /^satellite-/d | sed /-7-/d | sed /^ansible/d | grep rhel-8`"
-REPOS="`echo "$REPOS" | sed /^satellite-/d | sed /-sap-/d | sed /-7-/d | sed /-netweaver-/d | grep rhel-8`"
+REPOS="`echo "$REPOS" | sed /-sap-/d | sed /-7-/d | sed /^satellite-/d | sed /-debug-/d | grep rhel-8 | sed /-eus-/d | sed /rh-gluster-3/d`"
 
+
+REPOS="`echo "$REPOS" | sed /-7-/d`"
+REPOS="`echo "$REPOS" | sed /-debug-/d`"
+REPOS="`echo "$REPOS" | sed /-eus-/d`"
+REPOS="`echo "$REPOS" | sed /-sap-/d`"
+REPOS="`echo "$REPOS" | sed /^satellite-/d`"
+REPOS="`echo "$REPOS" | sed /rh-gluster-3/d`"
+REPOS="`echo "$REPOS" | sed /ansible-2.8-/d`"
+REPOS="`echo "$REPOS" | sed /ansible-2-/d`"
+
+set -o pipefail
 for repo in $REPOS; do
     echo
-    echo "$progname: mirroring $REPODIR/$repo"
-    echo "$progname: logging in $REPODIR/$repo.log"
-    nice reposync \
-        $REPOSYNCARGS \
-	--repoid=$repo 2>&1 2>&1 | tee $repo.log
+    rm -f /var/log/reposync/$repo.log
+    num=0
+    while [ $num -lt 10 ]; do
+	echo "$progname: mirroring $REPODIR/$repo"
+	echo "$progname: logging in /var/log/reposync/$repo.log"
+	nice reposync \
+             $REPOSYNCARGS \
+	     --repoid=$repo 2>&1 2>&1 | tee /var/log/reposync/$repo.log && break
+	num=`expr $num + 1`
+	echo "    Run $num failed, retrying"
+    done
+    rm -rf $repo/.repodata
+    nice createrepo $repo | tee -a /var/log/reposync/$repo.log
 done
 
 # Particularly useful for duplicate packages in storage and resilience channels
